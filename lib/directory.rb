97280f4f843bd8c9f8beb39f075cc063ee1b6c07
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-12 10:50:13 +0100

    file updated to include interactive menu

--- lib/directory.rb
+++ lib/directory.rb
@@ -1,20 +1,33 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array of students
-  students = []
+  puts "To finish, hit enter twice"
   # get first name
   name = gets.chomp
-  # while name is not empty, reapeat this code
+  # while 'exit' is not typed', reapeat this code
   while !name.empty? do
-    # add student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Student Cohort: "
+    cohort = gets.chomp.downcase
+    cohort == ""? cohort = :november : cohort = cohort.to_sym
+    
+    puts "Student Age: "
+    age = gets.chomp.to_i
+    
+    puts "Place of Birth: "
+    birth_place = gets.chomp
+    
+    puts "List hobbies with comma separation:"
+    hobbies = gets.chomp.split(",")
+    @students << {name: name, cohort: cohort, age: age, birth_place: birth_place, hobbies: hobbies}
+    
+    if @students.length != 1
+      puts "Now we have #{@students.count} students - add a name or hit enter to exit."
+    else
+      puts "Now we have 1 student - add a name or hit enter to exit."
+    end
+    
     # get another name from the user 
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
 def print_header
@@ -22,19 +35,58 @@ def print_header
   puts "----------------"
 end
 
-def print(students)
-  i = 0
-  until i == students.length do 
-    puts "#{(i+1)}: #{students[i][:name]} (#{students[i][:cohort]})"
-    i += 1
+def print_students
+  puts "Which cohort would you like to see?"
+  month = gets.downcase.delete("\n").to_sym
+  print_header
+  @students.map do |student|
+    if student[:cohort] == month
+      puts "#{student[:name]} - (#{student[:cohort]})"
+      puts "Age:".rjust(20) + " #{student[:age]}"
+      puts "Place of Birth:".rjust(20) + " #{student[:birth_place]}"
+      puts "Hobbies:".rjust(20) + " #{student[:hobbies].join(",")}"
+    end
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if @students.length >= 1
+    print_students
+    print_footer
+  else
+    puts "No students in directory"
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, ask again"
+  end
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
