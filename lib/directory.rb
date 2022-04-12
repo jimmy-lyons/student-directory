3d9b27cc3ce0d3d0a508f06398bd1c6b8bc34264
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-12 11:16:04 +0100

    File updated to save data to .csv file

--- lib/directory.rb
+++ lib/directory.rb
@@ -56,6 +56,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -74,11 +75,23 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you meant, ask again"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:age], student[:birth_place], student[:hobbies]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
